71e5a86af88c32075ad14c9426a6a2af71a11b39
Andrea La Rosa <andrea.larosa2@studio.unibo.it>
2026-03-25 18:39:22 +0100

    firs part done

--- src/main/scala/it/unibo/pps/u03/Sequences.scala
+++ src/main/scala/it/unibo/pps/u03/Sequences.scala
@@ -42,74 +42,113 @@ object Sequences: // Essentially, generic linkedlists
      * E.g., [10], [] => []
      * E.g., [], [] => []
      */
-    def zip[A, B](first: Sequence[A], second: Sequence[B]): Sequence[(A, B)] = ???
-
+    def zip[A, B](first: Sequence[A], second: Sequence[B]): Sequence[(A, B)] = (first, second) match
+      case (Nil(), _) => Nil()
+      case (_, Nil()) => Nil()
+      case (Cons(h1, t1), Cons(h2, t2)) => Cons((h1,h2), zip(t1,t2))
     /*
      * Concatenate two sequences
      * E.g., [10, 20, 30], [40, 50] => [10, 20, 30, 40, 50]
      * E.g., [10], [] => [10]
      * E.g., [], [] => []
      */
-    def concat[A](s1: Sequence[A], s2: Sequence[A]): Sequence[A] = ???
-
+    def concat[A](s1: Sequence[A], s2: Sequence[A]): Sequence[A] = s1 match
+      case Nil() => s2
+      case Cons(h,t) => Cons(h, concat(t, s2))
     /*
      * Reverse the sequence
      * E.g., [10, 20, 30] => [30, 20, 10]
      * E.g., [10] => [10]
      * E.g., [] => []
      */
-    def reverse[A](s: Sequence[A]): Sequence[A] = ???
-
+    def reverse[A](s: Sequence[A]): Sequence[A] =
+      def loop(rem: Sequence[A], acc: Sequence[A]): Sequence[A] = rem match
+        case Nil() => acc
+        case Cons(h, t) => loop(t, Cons(h, acc))
+      loop(s, Nil())
     /*
      * Map the elements of the sequence to a new sequence and flatten the result
      * E.g., [10, 20, 30], calling with mapper(v => [v, v + 1]) returns [10, 11, 20, 21, 30, 31]
      * E.g., [10, 20, 30], calling with mapper(v => [v]) returns [10, 20, 30]
      * E.g., [10, 20, 30], calling with mapper(v => Nil()) returns []
      */
-    def flatMap[A, B](s: Sequence[A])(mapper: A => Sequence[B]): Sequence[B] = ???
+    def flatMap[A, B](s: Sequence[A])(mapper: A => Sequence[B]): Sequence[B] = s match
+      case Nil() => Nil()
+      case Cons(h, t) => concat(mapper(h), flatMap(t)(mapper))
 
     /*
      * Get the minimum element in the sequence
      * E.g., [30, 20, 10] => 10
      * E.g., [10, 1, 30] => 1
      */
-    def min(s: Sequence[Int]): Optional[Int] = ???
+    def min(s: Sequence[Int]): Optional[Int] = s match {
+      case Nil() => Optional.Empty()
+      case Cons(h, Nil()) => Optional.Just(h)
+      case Cons(h, t) => min(t) match
+        case Optional.Just(min) => Optional.Just(math.min(h, min))
+        case Optional.Empty() => Optional.Just(h)
+    }
 
     /*
      * Get the elements at even indices
      * E.g., [10, 20, 30] => [10, 30]
      * E.g., [10, 20, 30, 40] => [10, 30]
      */
-    def evenIndices[A](s: Sequence[A]): Sequence[A] = ???
+    def evenIndices[A](s: Sequence[A]): Sequence[A] = s match {
+      case Nil() => Nil()
+      case Cons(h, Nil()) => Cons(h, Nil())
+      case Cons(h, Cons(_, t)) => Cons(h, evenIndices(t))
+    }
 
     /*
      * Check if the sequence contains the element
      * E.g., [10, 20, 30] => true if elem is 20
      * E.g., [10, 20, 30] => false if elem is 40
      */
-    def contains[A](s: Sequence[A])(elem: A): Boolean = ???
+    def contains[A](s: Sequence[A])(elem: A): Boolean = s match {
+      case Nil () => false
+      case Cons(h, t) if h == elem => true
+      case Cons(_, t) => contains(t)(elem);
+    }
 
     /*
      * Remove duplicates from the sequence
      * E.g., [10, 20, 10, 30] => [10, 20, 30]
      * E.g., [10, 20, 30] => [10, 20, 30]
      */
-    def distinct[A](s: Sequence[A]): Sequence[A] = ???
-
+    def distinct[A](s: Sequence[A]): Sequence[A] = s match
+      case Nil() => Nil()
+      case Cons(h, t) => Cons(h, distinct(filter(t)(_ != h)))
     /*
      * Group contiguous elements in the sequence
      * E.g., [10, 10, 20, 30] => [[10, 10], [20], [30]]
      * E.g., [10, 20, 30] => [[10], [20], [30]]
      * E.g., [10, 20, 20, 30] => [[10], [20, 20], [30]]
      */
-    def group[A](s: Sequence[A]): Sequence[Sequence[A]] = ???
+    def group[A](s: Sequence[A]): Sequence[Sequence[A]] = s match {
+      case Nil() => Nil()
+      case Cons(first, tail) => group(tail) match
+        case Nil() =>
+          Cons(Cons(first, Nil()), Nil())
+        case Cons(firstGroup, others) => firstGroup match
+          case Cons(firstElement, firstGroupTail) if first == firstElement =>
+            Cons(Cons(first, firstGroup), others)
+          case _ =>
+            Cons(Cons(first, Nil()), Cons(firstGroup, others))
+    }
 
     /*
      * Partition the sequence into two sequences based on the predicate
      * E.g., [10, 20, 30] => ([10], [20, 30]) if pred is (_ < 20)
      * E.g., [11, 20, 31] => ([20], [11, 31]) if pred is (_ % 2 == 0)
      */
-    def partition[A](s: Sequence[A])(pred: A => Boolean): (Sequence[A], Sequence[A]) = ???
+    def partition[A](s: Sequence[A])(pred: A => Boolean): (Sequence[A], Sequence[A]) = s match
+      case Nil() => (Nil(), Nil())
+      case Cons(h, t) => partition(t)(pred) match
+        case (satisfied, notSatisfied) =>
+          if pred(h) then (Cons(h, satisfied), notSatisfied)
+          else (satisfied, Cons(h, notSatisfied))
+
 
 @main def trySequences =
   import Sequences.* 
